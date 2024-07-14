f3b54de458ee4e4f32c373d3b276f5da9150ae78
Rishabh Jain <jainrishabh0525@gmail.com>
2024-07-14 09:26:56 +0530

    First commit

--- Day1/Demo.java
+++ Day1/Demo.java
@@ -1,6 +1,10 @@
 package Day1;
+import java.util.Scanner;
 public class Demo{
     public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
         System.out.println("Hi");
+        System.out.print(a);
     }
 }
